5cd8f70d7aeebed888713c59b198094a4a6f1087
PrashantK-hash <praskr065@yahoo.com>
2021-05-29 12:21:19 +0530

    Added secnat secnario test case

--- PromotionEngineSolution/PromotionEngine.UnitTests/BusinessRuleTests.cs
+++ PromotionEngineSolution/PromotionEngine.UnitTests/BusinessRuleTests.cs
@@ -20,5 +20,37 @@ namespace PromotionEngine.UnitTests
 
             Assert.AreEqual(100, result);
         }
+        [TestMethod]
+        public void CalculateTotalValueTest_ScenarioB()
+        {
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "A" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "A" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "A" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "A" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "A" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "B" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "B" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "B" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "B" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "B" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "C" }, PromotionId = 0 });
+            var bussinessRule = new BusinessRule();
+            var result = bussinessRule.CalculateTotalValue(shoppingCart);
+
+            Assert.AreEqual(370, result);
+        }
+        [TestMethod]
+        public void CalculateTotalValueTest_ScenarioC()
+        {
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "A" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "B" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "C" }, PromotionId = 0 });
+            var bussinessRule = new BusinessRule();
+            var result = bussinessRule.CalculateTotalValue(shoppingCart);
+
+            Assert.AreEqual(100, result);
+        }
     }
 }

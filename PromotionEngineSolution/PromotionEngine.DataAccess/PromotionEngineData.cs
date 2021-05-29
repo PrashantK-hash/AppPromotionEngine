b436469fa012047bcfbe8804a81c6668ef2356c0
PrashantK-hash <praskr065@yahoo.com>
2021-05-29 11:21:22 +0530

    Added get data from json file for SKU and promotional detail

--- PromotionEngineSolution/PromotionEngine.DataAccess/PromotionEngineData.cs
+++ PromotionEngineSolution/PromotionEngine.DataAccess/PromotionEngineData.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PromotionEngine.Common;
+using PromotionEngine.Entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +12,19 @@ namespace PromotionEngine.DataAccess
 {
     public class PromotionEngineData
     {
+        public List<PromotionDetails> GetPromotionDetails()
+        {
+            var appDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var filePath = Path.Combine(appDirectory, Constant.PromotionDetailsFilePath);
+            return JsonConvert.DeserializeObject<List<PromotionDetails>>(File.ReadAllText(filePath));
+        }
+
+        public List<SKU> GetSKUDetails()
+        {
+            var appDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var filePath = Path.Combine(appDirectory, Constant.AvailableSKUFilePath);
+            return JsonConvert.DeserializeObject<List<SKU>>(File.ReadAllText(filePath));
+        }
 
     }
 }

a0b2bceb5dff32dbfe3e8c51090d3bfb2e706cf7
PrashantK-hash <praskr065@yahoo.com>
2021-05-29 09:30:02 +0530

    Unit test Project

---
+++ PromotionEngineSolution/PromotionEngine.UnitTests/BusinessRuleTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PromotionEngine.UnitTests
+{
+    [TestClass]
+    public class BusinessRuleTests
+    {
+        [TestMethod]
+        public void CalculateTotalValueTest_ScenarioA()
+        {
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "A" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "B" }, PromotionId = 0 });
+            shoppingCart.ShoppingCartItem.Add(new ShoppingCartItem() { PromotionApplied = false, SKU = new SKU() { Name = "C" }, PromotionId = 0 });
+            var bussinessRule = new BusinessRule();
+            var result = bussinessRule.CalculateTotalValue(shoppingCart);
+
+            Assert.AreEqual(100, result);
+        }
+    }
+}
---
+++ PromotionEngineSolution/PromotionEngine.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PromotionEngine.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PromotionEngine.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63a670b9-2ccc-4068-af3d-5e17a74d58ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

7a3dad11f8f5f6e3403ba186cca3eba862496f35
PrashantK-hash <praskr065@yahoo.com>
2021-05-29 10:29:23 +0530

    Added constant class for json location

---
+++ PromotionEngineSolution/PromotionEngine.Common/Constant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Common
+{
+    public class Constant
+    {
+        public const string AvailableSKUFilePath = @"AppData\SKUDetails.json";
+        public const string PromotionDetailsFilePath = @"AppData\PromotionDetails.json";
+
+    }
+}
